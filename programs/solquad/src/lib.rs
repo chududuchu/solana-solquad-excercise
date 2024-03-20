a23112bd580d3c3c53784d27d6ea2ea5490cfaf4
chududuchu <164136625+chududuchu@users.noreply.github.com>
2024-03-20 13:18:32 +0700

    Update lib.rs

--- programs/solquad/src/lib.rs
+++ programs/solquad/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("5sFUqUTjAMJARrEafMX8f4J1LagdUQ9Y8TR8HwGNHkU8");
+declare_id!("AWpdzq4XYQxaMbMhpUgNta6KNdrRGzfDiMhwLL7qLDZ1");
 
 #[program]
 pub mod solquad {
